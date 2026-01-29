3a1d062e30449eaf5fd90d7be83c34548354cb6f
nvthong05-hash <nvthong05@gmail.com>
2026-01-30 00:18:02 +0700

    test 2

--- zzzz/zzzz/MVVM/ViewModels/Login.cs
+++ zzzz/zzzz/MVVM/ViewModels/Login.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace zzzz.MVVM.ViewModels
 {
     internal class Login
     {
+        public ICommand LoginCommand => new RoutedCommand();
     }
 }
